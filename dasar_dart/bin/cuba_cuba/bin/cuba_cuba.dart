cab878f8bd6d2bba80ae9d9eba39894d347da370
Gina <ginasahiba@gmail.com>
2021-03-08 17:38:34 +0700

    cuba

---
+++ dasar_dart/bin/cuba_cuba/bin/cuba_cuba.dart
@@ -0,0 +1,5 @@
+import 'package:cuba_cuba/cuba_cuba.dart' as cuba_cuba;
+
+void main(List<String> arguments) {
+  print('Hello world: ${cuba_cuba.calculate()}!');
+}
---
+++ dasar_dart/bin/cuba_cuba/test/cuba_cuba_test.dart
@@ -0,0 +1,8 @@
+import 'package:cuba_cuba/cuba_cuba.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
