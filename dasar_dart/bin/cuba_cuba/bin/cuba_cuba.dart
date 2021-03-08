c54b2db948e28e7651e7d7c4543b96c09e5268c0
GinaSahiba <80249211+GinaSahiba@users.noreply.github.com>
2021-03-08 17:43:10 +0700

    Delete dasar_dart/bin directory

--- dasar_dart/bin/cuba_cuba/bin/cuba_cuba.dart
+++
@@ -1,5 +0,0 @@
-import 'package:cuba_cuba/cuba_cuba.dart' as cuba_cuba;
-
-void main(List<String> arguments) {
-  print('Hello world: ${cuba_cuba.calculate()}!');
-}
--- dasar_dart/bin/cuba_cuba/test/cuba_cuba_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:cuba_cuba/cuba_cuba.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
